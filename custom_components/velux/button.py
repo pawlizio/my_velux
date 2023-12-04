d0db40ba69b2a0cca3c78a1aa7d2f96305cf0a11
Paul Daumlechner <paul.daumlechner@live.de>
2023-12-04 21:33:06 +0100

    update light.py

--- custom_components/velux/button.py
+++ custom_components/velux/button.py
@@ -1,63 +1,64 @@
 """Component to pressing a button as platforms."""
 
-from pyvlx.opening_device import Blind
+from pyvlx import PyVLX
 
-from homeassistant.helpers.entity import EntityCategory
-from homeassistant.components.button import ButtonEntity, ButtonDeviceClass
+from homeassistant.components.button import ButtonDeviceClass, ButtonEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import DeviceInfo, EntityCategory
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN
 
 PARALLEL_UPDATES = 1
 
 
-async def async_setup_entry(hass, entry, async_add_entities):
+async def async_setup_entry(
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+) -> None:
     """Set up sensor(s) for Velux platform."""
     entities = []
-    gateway = hass.data[DOMAIN][entry.entry_id]
-    entities.append(VeluxGatewayRestart(gateway))
+    pyvlx: PyVLX = hass.data[DOMAIN][entry.entry_id]
+    entities.append(VeluxGatewayRestart(pyvlx))
     async_add_entities(entities)
 
 
 class VeluxGatewayRestart(ButtonEntity):
-    """Representation of a Velux number."""
+    """Representation of a KLF200 restart button entity."""
 
-    def __init__(self, gateway):
-        """Initialize the cover."""
-        self.pyvlx = gateway
-        # self.entity_description = "Configure open orientation of %s", self.node.name
+    def __init__(self, pyvlx: PyVLX) -> None:
+        """Initialize the button entity."""
+        self.pyvlx: PyVLX = pyvlx
 
     @property
-    def name(self):
-        """Name of the entity."""
+    def name(self) -> str:
+        """Name of the button entity."""
         return "KLF200 Reboot"
 
     @property
-    def device_info(self):
+    def device_info(self) -> DeviceInfo:
+        """Return specific device attributes."""
         return {
-            "identifiers": {
-                (DOMAIN, self.unique_id)
-            },
-            "connections": {
-                ("Host", self.pyvlx.config.host)
-            },
+            "identifiers": {(DOMAIN, self.unique_id)},
+            "connections": {("Host", self.pyvlx.config.host)},
             "name": "KLF200 Gateway",
             "manufacturer": "Velux",
             "sw_version": self.pyvlx.version,
         }
 
     @property
-    def unique_id(self):
-        """Return the unique ID of this cover."""
+    def unique_id(self) -> str:
+        """Return the unique ID."""
         return "KLF200_Reboot"
 
     @property
-    def entity_category(self):
-        """Return the entity_categor of this number."""
+    def entity_category(self) -> EntityCategory:
+        """Return the entity category."""
         return EntityCategory.CONFIG
 
     @property
-    def device_class(self):
-        """Return the entity_categor of this number."""
+    def device_class(self) -> ButtonDeviceClass:
+        """Return the device class."""
         return ButtonDeviceClass.RESTART
 
     async def async_press(self) -> None:
